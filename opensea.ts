84abee75e5f80bfd0f3812d3ff556c5f035f7d60
Androz2091 <androz2091@gmail.com>
2021-10-12 21:29:26 +0200

    :sparkles: Add ratelimit

--- opensea.ts
+++ opensea.ts
@@ -1,18 +1,25 @@
 import fetch from 'node-fetch';
+import { pRateLimit } from 'p-ratelimit';
+
+const limit = pRateLimit({
+    interval: 200, 
+    rate: 1,
+    concurrency: 1, 
+    maxDelay: 2000
+});
 
 export const getCollectionStats = (slug: string): Promise<any> => {
 
-    return new Promise((resolve) => {
+    return new Promise(async (resolve) => {
 
-        fetch(`https://api.opensea.io/collection/${slug}`).then((res) => {
+        await limit(() => fetch(`https://api.opensea.io/collection/${slug}`).then((res) => {
 
             res.json().then((data) => {
 
                 resolve(data);
 
             });
-
-        });
+        }));
 
     });
 
